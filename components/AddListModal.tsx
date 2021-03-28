0eb4e49fdbed16b02d8e6b7276370315510933b5
faisalsayed10 <faisal.sayed502@gmail.com>
2021-03-28 16:22:07 +0530

    AddListModal

---
+++ components/AddListModal.tsx
@@ -0,0 +1,85 @@
+import { Button } from "@chakra-ui/button";
+import { FormControl, FormLabel } from "@chakra-ui/form-control";
+import { useDisclosure } from "@chakra-ui/hooks";
+import { Input } from "@chakra-ui/input";
+import {
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+} from "@chakra-ui/modal";
+import { useToast } from "@chakra-ui/toast";
+import axios from "axios";
+import { useUser } from "hooks";
+import React from "react";
+import { useForm } from "react-hook-form";
+import useSWR, { mutate } from "swr";
+
+interface Props {}
+
+const AddListModal: React.FC<Props> = ({}) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const toast = useToast();
+  const { handleSubmit, register } = useForm();
+  const { user } = useUser();
+  const { data } = useSWR("/api/list");
+
+  const onCreate = async ({ name }) => {
+    try {
+      const { data } = await axios.post("/api/list", { name });
+      toast({
+        title: "Success!",
+        description: "List created",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+      });
+      // mutate(
+      //   ["/api/lists", user.id],
+      //   async (data) => ({ sites: [{ id, ...newSite }, ...data.sites] }),
+      //   false
+      // );
+      onClose();
+    } catch (err) {
+      console.error(err)
+    }
+  };
+
+  return (
+    <>
+      <Button variant="ghost" onClick={onOpen}>
+        Create A List
+      </Button>
+      <Modal size="xl" isCentered={true} isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent as="form" onSubmit={handleSubmit(onCreate)}>
+          <ModalHeader fontWeight="bold">Create A List</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <FormControl mt={4}>
+              <FormLabel>List Name</FormLabel>
+              <Input
+                placeholder="My new list"
+                name="name"
+                ref={register({ required: "Required" })}
+              />
+            </FormControl>
+          </ModalBody>
+          <ModalFooter>
+            <Button onClick={onClose} mr={3} fontWeight="medium">
+              Cancel
+            </Button>
+            <Button backgroundColor="#99FFFE" color="#194D4C" type="submit">
+              Add
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default AddListModal;
